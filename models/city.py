a836192acd8179385d72dc80103f6befb4f3431a
James Koduah <jameskoduah171@gmail.com>
2023-04-21 15:09:56 +0000

    city.py: task 6: first draft

--- models/city.py
+++ models/city.py
@@ -3,7 +3,10 @@
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    name = Column(String(128))
+    state_id = Column(String(60), ForeignKey('states.id'))
     state_id = ""
     name = ""
